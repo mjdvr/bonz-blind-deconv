daf5a50367377c0e520a223231152aa961641b04
mjdvr <mjdevroome96@gmail.com>
2022-10-19 11:43:42 +0200

    Applied a gaussian distortion, genereated its magnitude spectrum and displayed it alongside the original for comparison.

--- step01/README.md
+++ step01/README.md
@@ -3,6 +3,6 @@ Folder in which the first steps are taken towards the end goal.
 In this step:
  - &check; read image
  - &check; create a fourier transform of this image
- - generate a transform mask
- - apply mask to image, thus creating a convoluted image
+ - &check; generate a transform mask (automated through the *imgaug* package)
+ - &check; apply mask to image, thus creating a convoluted image
  - deconvolve the image with a simple scheme, Wiener deconvolution
--- step01/step01.py
+++ step01/step01.py
@@ -2,19 +2,35 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+from imgaug import augmenters as iaa
 
 # make the plots more comprehensive
-fig, ax = plt.subplots(1, 2, figsize=(9, 6))
+fig, ax = plt.subplots(2, 2, figsize=(10, 10))
 
 # import and show image
 f = cv.imread('./img/alphabet/a.png', 0)
-ax[0].imshow(f, cmap = 'gray')
-ax[0].set_title("source image")
+ax[0,0].imshow(f, cmap = 'gray')
+ax[0,0].set_title("source image (f)")
 
 # create fourier transform of the loaded image
 f_fourier = np.fft.fft2(f)
 f_shift = np.fft.fftshift(f_fourier)
 f_mag_spec = 20*np.log(np.abs(f_shift))
-ax[1].imshow(f_mag_spec, cmap = 'gray')
-ax[1].set_title("magnitude spectrum")
+ax[1,0].imshow(f_mag_spec, cmap = 'gray')
+ax[1,0].set_title("magnitude spectrum (f)")
+
+# generate a gaussian transform mask, and create the distorted image
+aug_gauss = iaa.AdditiveGaussianNoise(scale=(10, 60), seed=1)
+b = aug_gauss(image=f)
+ax[0,1].imshow(b, cmap = 'gray')
+ax[0,1].set_title("distorted image (b)")
+
+# foerier transform, shift and create a magnitude spectrum as above
+b_fourier = np.fft.fft2(b)
+b_shift = np.fft.fftshift(b_fourier)
+b_mag_spec = 20*np.log(np.abs(b_shift))
+ax[1,1].imshow(b_mag_spec, cmap = 'gray')
+ax[1,1].set_title("magnitude spectrum (b)")
+
+# show the plots
 plt.show()
