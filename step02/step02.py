1f424be4ba6558be39fe3e97675a99070370359c
mjdvr <mjdevroome96@gmail.com>
2022-10-31 12:51:16 +0100

    Significantly changed the deconvolution algorithm, normalized the f and g each iteration, added function to accept more general input g.

--- step02/step02.py
+++ step02/step02.py
@@ -5,7 +5,7 @@ from skimage import color, data, restoration
 from scipy.signal import convolve2d as conv2
 
 # make the plots more comprehensive
-fig, ax = plt.subplots(2, 10, figsize=(10, 7))
+fig, ax = plt.subplots(2, 10, figsize=(18, 7))
 
 def matlab_style_gauss2D(shape=(5,5),sigma=1):
     # source: https://stackoverflow.com/a/17201686
@@ -22,7 +22,18 @@ def matlab_style_gauss2D(shape=(5,5),sigma=1):
         h /= sumh
     return h
 
+def g_reshape(g, goal):
+    """ this function take the psf and reshapes it to a size that matches that of the image """
+    x_pad_affix = int(np.floor((goal[0]-g.shape[0])/2))
+    x_pad_suffix = int(goal[0]-g.shape[0]-x_pad_affix)
+    y_pad_affix = int(np.floor((goal[1]-g.shape[1])/2))
+    y_pad_suffix = int(goal[1]-g.shape[1]-y_pad_affix)
+    g = np.pad(g, ((x_pad_affix, x_pad_suffix),(y_pad_affix, y_pad_suffix)), 'constant')
+    return g
+
+
 def normalize_complex_arr(a):
+    # source: https://stackoverflow.com/a/41576956
     a_oo = a - a.real.min() - 1j*a.imag.min() # origin offsetted
     return a_oo/np.abs(a_oo).max()
 
@@ -32,13 +43,9 @@ def conv_prod(a, b):
 
 def f_loop(f, g, c, nr_iter):
     """ this function takes an initial f, g and c, and returns the new f after 'nr_iter' RL iterations """
-    if g.shape != (100,100):
+    if g.shape != f.shape:
         # reshape and center the psf to match the size of the image to make convolution possible
-        x_pad_affix = int(np.floor((100-g.shape[0])/2))
-        x_pad_suffix = int(100-g.shape[0]-x_pad_affix)
-        y_pad_affix = int(np.floor((100-g.shape[1])/2))
-        y_pad_suffix = int(100-g.shape[1]-y_pad_affix)
-        g = np.pad(g, ((x_pad_affix, x_pad_suffix),(y_pad_affix, y_pad_suffix)), 'constant')
+        g = g_reshape(g, f.shape)
 
     for i in range(nr_iter):
         f = conv_prod(c/conv_prod(f,g),np.flip(g))*f
@@ -49,13 +56,9 @@ def f_loop(f, g, c, nr_iter):
 
 def g_loop(f, g, c, nr_iter):
     """ this function takes an initial f, g and c, and returns the new g after 'nr_iter' RL iterations """
-    if g.shape != (100,100):
+    if g.shape != f.shape:
         # reshape and center the psf to match the size of the image to make convolution possible
-        x_pad_affix = int(np.floor((100-g.shape[0])/2))
-        x_pad_suffix = int(100-g.shape[0]-x_pad_affix)
-        y_pad_affix = int(np.floor((100-g.shape[1])/2))
-        y_pad_suffix = int(100-g.shape[1]-y_pad_affix)
-        g = np.pad(g, ((x_pad_affix, x_pad_suffix),(y_pad_affix, y_pad_suffix)), 'constant')
+        g = g_reshape(g, f.shape)
 
     for i in range(nr_iter):
         g = conv_prod(c/conv_prod(g,f),np.flip(f))*g
@@ -69,11 +72,11 @@ def RL_decon(init_guess=1, nr_iter=5):
     return a
 
 # import and show image
-f = cv.imread('./img/alphabet/b.png', 0)
+f = cv.imread('./img/alphabet/a.png', 0)
 psf = matlab_style_gauss2D(shape=(5,5), sigma=1)
 image = conv2(f, psf, 'same')
 
-psf = matlab_style_gauss2D(shape=(75,5), sigma=10)
+psf = matlab_style_gauss2D(shape=(2,50), sigma=10)
 #psf = np.ones((100, 100)) / 25
 f_loop(image,psf,image,10)
 g_loop(image,psf,image,10)
